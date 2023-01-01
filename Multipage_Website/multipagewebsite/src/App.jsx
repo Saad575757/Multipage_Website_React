37d60029309f602a7affc77d446f59786e03ce6d
Saad575757 <smartysaad59@gmail.com>
2023-01-01 21:22:10 +0500

    done

--- Multipage_Website/multipagewebsite/src/App.jsx
+++ Multipage_Website/multipagewebsite/src/App.jsx
@@ -3,14 +3,20 @@ import About from './About'
 import Contact from './Contact'
 import Home from './Home'
 import Services from './Services'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      <Home />
-      <About />
-      <Services />
-      <Contact />
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/services' element={<Services/>}/>
+      <Route path='/contact' element={<Contact />}/>
+      <Route path='*' element={} />
+     </Routes>
+     </BrowserRouter>
     </>
   )
 }
