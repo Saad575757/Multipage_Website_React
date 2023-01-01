63cb95011a46dec8983be5cc55ad002033b37465
Saad575757 <smartysaad59@gmail.com>
2023-01-02 03:46:22 +0500

    Great work today

--- Multipage_Website/multipagewebsite/src/App.jsx
+++ Multipage_Website/multipagewebsite/src/App.jsx
@@ -4,50 +4,50 @@ import Contact from './Contact'
 import Home from './Home'
 import Services from './Services'
 import Error from './Error'
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import './App.css'
-import {ThemeProvider} from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 const App = () => {
-  const theme={
-    colors:{
+  const theme = {
+    colors: {
       heading: "rgb(24 24 29)",
       text: "rgb(24 24 29)",
-      white:"#fff",
-      black:" #212529",
+      white: "#fff",
+      black: " #212529",
       bg: "rgb(249 ,249 ,255)",
-      footer_bg:"#0a1435",
-      btn:"rgb(98 84 243)",
-      border:"rgba(98, 84, 243,0.5)",
-      hr:"#ffffff",
-      helper:"#8490ff",
+      footer_bg: "#0a1435",
+      btn: "rgb(98 84 243)",
+      border: "rgba(98, 84, 243,0.5)",
+      hr: "#ffffff",
+      helper: "#8490ff",
       gradient: "linear-gradient(0deg,rgb(132,144,255)0%,rgb(98 189 252) 100%)",
       shadow: "rgba(0,0,0,0.2) 0px 1px 3px 0px, rgba(27,31,35,0.15) 0px 0px 0px 1px;",
-      shadowSupport:"rgba(0,0,0,0.16) 0px 1px 4px",
+      shadowSupport: "rgba(0,0,0,0.16) 0px 1px 4px",
 
-      
+
 
     },
-    media:{mobile:"768", tab:"998",},
-    
+    media: { mobile: "768", tab: "998", },
+
   }
   return (
     <>
-     <ThemeProvider theme={theme}>
-     <Header />
-     <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/about' element={<About />}/>
-      <Route path='/service' element={<Services/>}/>
-      <Route path='/contact' element={<Contact />}/>
-      <Route path='*' element={<Error />} />
-     </Routes>
-     <Footer />
-     </ThemeProvider>
-     
-     
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/service' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
+
+
     </>
   )
 }
