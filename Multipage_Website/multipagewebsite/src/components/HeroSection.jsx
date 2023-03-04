8a3a80d5016b93ccc7c74a984d771680ab7415aa
Saad575757 <smartysaad59@gmail.com>
2023-03-04 19:51:09 +0500

    done

--- Multipage_Website/multipagewebsite/src/components/HeroSection.jsx
+++ Multipage_Website/multipagewebsite/src/components/HeroSection.jsx
@@ -5,7 +5,15 @@ const HeroSection = () => {
   return (
     <>
       <Wrapper>
-        <div className="container grid grid-two-column"></div>
+        <div className="container grid grid-two-column">
+          <div className="section-data-hero">
+            <p>This is me</p>
+            <h1 className='heading'>Saad</h1>
+            <p className='Paragraph'>I'm Frontend Webdeveloper and App developer.</p>
+
+          </div>
+          <div className="section-data-image"></div>
+        </div>
       </Wrapper>
     </>
   )
