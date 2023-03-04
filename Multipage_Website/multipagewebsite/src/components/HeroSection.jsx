15f92066081836f605df3fe4dc9524d0de3ae852
Saad575757 <smartysaad59@gmail.com>
2023-03-04 19:45:11 +0500

    done

--- Multipage_Website/multipagewebsite/src/components/HeroSection.jsx
+++ Multipage_Website/multipagewebsite/src/components/HeroSection.jsx
@@ -4,22 +4,9 @@ import styled from 'styled-components'
 const HeroSection = () => {
   return (
     <>
-    <Wrapper>
-        <div className="container-grid-two-column">
-            <div className="section-hero-data">
-                <p className='paragraoh'>This is me</p>
-                <h1 className='heading'>Saad Khan</h1>
-                <p className='intro'>I'm Frontend ReactJs Web-developer and flutter App devolper,
-                    working as freelancer.
-                </p>
-
-            </div>
-            <Button>
-                 
-            </Button>
-            <div className="section-hero-img"></div>
-        </div>
-    </Wrapper>
+      <Wrapper>
+        <div className="container grid grid-two-column"></div>
+      </Wrapper>
     </>
   )
 }
