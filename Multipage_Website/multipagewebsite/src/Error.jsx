d8b4d7d4a7e99d85e9ba47672612f6b582b5ff81
Saad575757 <smartysaad59@gmail.com>
2023-03-17 00:56:17 +0500

    ok

--- Multipage_Website/multipagewebsite/src/Error.jsx
+++ Multipage_Website/multipagewebsite/src/Error.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
-import styled from 'styled-components'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { Button } from './styles/Button';
 
 const Error = () => {
   return (
+    
     <Wrapper>
-
+      <h2>No Page Found</h2>
+      <Button className='btn'>
+              <NavLink to='/'>Go Back</NavLink>
+            </Button>
     </Wrapper>
+      
+   
+    
   )
 }
 const Wrapper=styled.section`
+  padding: 15rem 0;
+  
+  .btn{
+    margin-top: 3rem;
+    margin-left: 45%;
+  }
 `;
-export default Error
+
+export default Error;
