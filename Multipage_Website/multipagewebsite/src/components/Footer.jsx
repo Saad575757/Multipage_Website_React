3f41f86df385bd02bd8386d3ba4431cd3a7e3b22
Saad575757 <smartysaad59@gmail.com>
2023-03-20 18:48:20 +0500

    great tho

--- Multipage_Website/multipagewebsite/src/components/Footer.jsx
+++ Multipage_Website/multipagewebsite/src/components/Footer.jsx
@@ -4,7 +4,12 @@ import styled from 'styled-components'
 const Footer = () => {
   return (
     <Wrapper>
-      <div className="contact-short"></div>
+      <div className="contact-short">
+        <div>
+          <h3>Ready to get started?</h3>
+          <h3>Talk us today</h3>
+        </div>
+      </div>
     </Wrapper>
   )
 }
